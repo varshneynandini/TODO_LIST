af6b9ab8051b12255e833a5ae16c312c504c227e
varshneynandini <varshneynandini797@gmail.com>
2024-06-16 20:01:29 +0530

    Add files via upload

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from .views import home, login, signup
+
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
+]
