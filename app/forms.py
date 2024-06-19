040ab48e2c3c94abeb11c0fc585e914e27f4e5ce
varshneynandini <varshneynandini797@gmail.com>
2024-06-19 21:15:01 +0530

    Delete app directory

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from app.models import TODO
-
-class TODOForm(ModelForm):
-    class Meta:
-        model = TODO
-        fields = ['title', 'status', 'priority']
